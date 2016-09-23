2f4a9e020f12aae4fafe3559b3d23698c99b2e78
fikyfjw <vq.rhyme@gmail.com>
2016-09-23 17:28:30 +0700

    update implementasi compare #2 compare content file

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,18 +1,44 @@
 package main
 
 import (
+	_ "encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type wilayah struct {
+	Data []interface{} `json:"data"`
+}
+
+/*type dataWilayah struct {
+	kode_wilayah     string
+	nama             string
+	mst_kode_wilayah string
+}*/
+
+var (
+	apiMuseum  = "http://jendela.data.kemdikbud.go.id/api/index.php/CcariMuseum/searchGET"
+	apiWilayah = "http://jendela.data.kemdikbud.go.id/api/index.php/CWilayah/wilayahGET"
 )
 
 func main() {
-	flag.IntVar(&maxGoroutinesSpawn, "max", 1, "max goroutines")
-	flag.StringVar(&targetFile, "target", "./output.txt", "target save file")
-	flag.Parse()
+	// var data wilayah
+	// var data1 map[string]interface{}
+	client := &http.Client{}
 
-	if rootUrl == "" {
-		fmt.Println("Cannot nil URL Parameter")
-		os.Exit(-1)
-	} else {
-		getSiteURL(rootUrl, maxGoroutinesSpawn, targetFile)
-	}
+	response, _ := client.Get(apiWilayah)
+	// request, _ := http.NewRequest("GET", apiWilayah, nil)
+
+	// response, _ := client.Do(request)
+
+	defer response.Body.Close()
+
+	respBody, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println("data :", string(respBody))
+
+	// for _, v := range data {
+	// 	fmt.Printf("Kode Wilayah: %s\t Nama: %s\t Mst Kode Wilayah:	%s\n", data.kode_wilayah, data.nama, data.mst_kode_wilayah)
+	// }
 }
